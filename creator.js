1670248c216deea82bb81b0e5271ad29711458f2
Philip Bjorge <philipbjorge@gmail.com>
2013-03-06 23:17:25 -0800

    Finished initial REPL support. Code execution mostly supported (see #3).

--- creator.js
+++ creator.js
@@ -3,6 +3,20 @@ var creator_init = {
 		editor = ace.edit("editor");
 		editor.setTheme("ace/theme/monokai");
 		editor.getSession().setMode("ace/mode/ruby");
+		editor.commands.addCommand({
+		    name: 'repl',
+		    bindKey: {win: 'Ctrl-R',  mac: 'Command-R'},
+		    exec: function(editor) {
+		    	repl_reload = true;
+		    	var old_line = repl.GetPromptText();
+		    	repl.ClearPromptText();
+		    	repl.SetPromptText(editor.getValue());
+		    	repl._HandleEnter()
+		    	repl.ClearPromptText();
+		    	repl.SetPromptText(old_line);
+		    },
+		    readOnly: true // false if this command should not apply in readOnly mode
+		});
 	},
 
 	layout: function() {
@@ -22,19 +36,40 @@ var creator_init = {
         repl.RegisterMatching('{', '}', 'jqconsole-brackets');
         repl.RegisterMatching('[', ']', 'jqconsole-braces');
         repl.RegisterMatching('(', ')', 'jqconsole-parens');
+
+        $("#repl-exec").click(function() {
+        	editor.commands.commands.repl.exec(editor);
+        });
+
 		// Output input with the class jqconsole-output.
 		var old_console_log = console.log;
-		console.log = function(args) { old_console_log.apply(this, arguments); repl.Write(Array.prototype.join.call(arguments, ' ') + "\n", 'jqconsole-output'); };
+		var last_printed_count = 0;
+		console.log = function(args) { 
+			old_console_log.apply(this, arguments);
+			if (last_printed_count <= 0) {
+				repl.Write(Array.prototype.join.call(arguments, ' ').substring(last_printed_count) + "\n", 'jqconsole-output');
+			}
+			last_printed_count -= Array.prototype.join.call(arguments, ' ').length + 1;
+		};
 
         var startPrompt = function (repl_input) {
           // Start the prompt with history enabled.
           repl.Prompt(true, function (input) {
+          	if (typeof(repl_reload) === "undefined" || repl_reload) {
+          		repl_input = "";
+          		last_printed_count = 0;
+          		repl_reload = false;
+          	}
+
             try {
+            	var old_last_printed_count = last_printed_count;
             	repl.Write(Move.eval(repl_input + "\n" + input) + '\n', 'jqconsole-output-repl');
+            	last_printed_count = old_last_printed_count + (-1 * last_printed_count);
             	repl_input += "\n" + input;
         	}
         	catch (err) {
         		repl.Write(err + '\n', 'jqconsole-error');
+        		last_printed_count = old_last_printed_count;
         	}
             // Restart the prompt.
             startPrompt(repl_input);
